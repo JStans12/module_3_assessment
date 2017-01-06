c340834d74cc5b8aeef8fc5c9188b6d7cded51f1
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-06 09:29:22 -0700

    all api routes work

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,29 @@ class Api::V1::ItemsController < ApplicationController
     items = Item.all
     render json: items, Serializer: ItemSerializer
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy!
+      render json: "Item #{params[:id]} will not bother you anymore...", status: 240
+    end
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    end
+  end
+
+  private
+
+    def item_params
+      params.permit(:name, :description, :image_url)
+    end
 end
---
+++ spec/api_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe "api" do
+  it "items index" do
+    
+  end
+end
