987858a989413b6a5c1b4c851d59cc207f4310d4
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-06 12:00:09 -0700

    figured out self api testing

--- spec/controllers/api_spec.rb
+++ spec/controllers/api_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 describe Api::V1::ItemsController do
-  xit "items index" do
+  it "items index" do
+    item = Item.create(name: "j", description: "k", image_url: "l")
+    item = Item.create(name: "m", description: "n", image_url: "o")
+    item = Item.create(name: "p", description: "q", image_url: "r")
+
+    get :index
+    items = JSON.parse(response.body, symbolize_names: true)
+    item = items.first
+
+    expect(items.length).to eq(3)
+    expect(item).to have_key(:name)
+    expect(item).to have_key(:description)
+    expect(item).to have_key(:image_url)
   end
 end
