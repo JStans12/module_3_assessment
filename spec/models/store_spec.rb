6e4607b82e0b1ecb3d2e35dd222024d642487754
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-06 10:43:51 -0700

    store model done and tested

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -9,5 +9,10 @@ describe Store do
 
     expect(response[0]).to eq(16)
     expect(stores.length).to eq(15)
+    expect(store).to respond_to(:store_type)
+    expect(store).to respond_to(:long_name)
+    expect(store).to respond_to(:city)
+    expect(store).to respond_to(:state)
+    expect(store).to respond_to(:phone)
   end
 end
