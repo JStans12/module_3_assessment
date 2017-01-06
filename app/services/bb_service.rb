0845002598c23cfc3c69ee8532d16738ad9e3275
Joey Stansfield <joseph.r.stansfield@gmail.com>
2017-01-06 11:24:19 -0700

    store by id service works

--- app/services/bb_service.rb
+++ app/services/bb_service.rb
@@ -4,4 +4,9 @@ class BbService
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeId,storeType,phone,distance,longName,city,region&apiKey=a8k7nnexb87aa9hb3zzqbgf7&pageSize=15")
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.by_id(id)
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(storeId=#{id})?format=json&show=storeId,hours,address,longName,storeType,phone,name,city,region&apiKey=a8k7nnexb87aa9hb3zzqbgf7")
+    parsed = JSON.parse(response.body, symbolize_names: true)[:stores][0]
+  end
 end
--- spec/services/bb_service_spec.rb
+++ spec/services/bb_service_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe BbService do
   it "#get_stores" do
 
-
     response = BbService.get_stores(80202)
     stores = response[:stores]
     store = stores.first
@@ -16,5 +15,18 @@ describe BbService do
     expect(store).to have_key(:region)
     expect(store).to have_key(:phone)
     expect(store).to have_key(:distance)
+  end
+
+  it "#by_id" do
+
+    store = BbService.by_id(2387)
+
+    expect(store).to have_key(:storeType)
+    expect(store).to have_key(:longName)
+    expect(store).to have_key(:city)
+    expect(store).to have_key(:region)
+    expect(store).to have_key(:phone)
+    expect(store).to have_key(:hours)
+    expect(store).to have_key(:address)
   end
 end
